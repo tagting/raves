9462c73c0dc3b620a751114c6c0d376fb07432db
ting <ttzhenng@gmail.com>
2021-03-06 16:28:39 -0500

    database changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ User.create(name: "Joon", username: "joonkim", email: "joonkim1112@gmail.com")
 User.create(name: "Kate", username: "katie_foodie", email: "kate_smith@yahoo.com")
 User.create(name: "Alice", username: "ally00n", email: "alicemariethomas@site.edu")
 
-Review.create(title: "Amazing Food", content: "Amazing Food and Drinks!", rating: 5, user_id: 1)
+Review.create(title: "Amazing Food", content: "Amazing Food and Drinks!", rating: 5, user_id: 1, restaurant_id: 1)
 
 10.times do
     Restaurant.create(
