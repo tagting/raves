67aa0fcf5bc384e274a22de4f2920db1bbe121af
ting <ttzhenng@gmail.com>
2021-03-06 14:50:46 -0500

    make changes to reviews table

---
+++ db/migrate/20210306194909_add_userid_to_reviews.rb
@@ -0,0 +1,5 @@
+class AddUseridToReviews < ActiveRecord::Migration[5.2]
+  def change
+    add_column :reviews, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_193749) do
+ActiveRecord::Schema.define(version: 2021_03_06_194909) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_03_06_193749) do
     t.string "title"
     t.text "content"
     t.integer "rating"
+    t.datetime "timestamp"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
