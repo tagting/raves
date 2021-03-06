ed4e8aac440dd9925fc841f37cd97f5855c75c2b
ting <ttzhenng@gmail.com>
2021-03-06 17:28:25 -0500

    fixed error: missing 'end'

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,8 @@
 class ReviewsController < ApplicationController
 
-#user requesting all reviews
-get '/reviews' do
-    @reviews = Review.all
-    erb :'reviews/index'
+    #user requesting all reviews
+    get '/reviews' do
+        @reviews = Review.all
+        erb :'reviews/index'
+    end
 end
