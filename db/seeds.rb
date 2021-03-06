b3fab5e2b6c539cffc4d32a2b11d4f6f61994c58
ting <ttzhenng@gmail.com>
2021-03-06 16:01:15 -0500

    edit seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
-Restauarant.destroy_all
+User.create(name: "Nick", username: "nickeats", email: "nickeats@website.com")
+User.create(name: "Joon", username: "joonkim", email: "joonkim1112@gmail.com")
+User.create(name: "Kate", username: "katie_foodie", email: "kate_smith@yahoo.com")
+User.create(name: "Alice", username: "ally00n", email: "alicemariethomas@site.edu")
 
+Review.create(title: "Amazing Food", content: "Amazing Food and Drinks!", rating: 5, user_id: 1)
 
 10.times do
     Restaurant.create(
@@ -7,10 +11,3 @@ Restauarant.destroy_all
         address: Faker::Address.street_address
     )
 end
-
-User.create(name: "Nick", username: "nickeats", password: "password", email: "nickeats@website.com")
-User.create(name: "Joon", username: "joonkim", password: "password", email: "joonkim1112@gmail.com")
-User.create(name: "Kate", username: "katie_foodie", password: "password", email: "kate_smith@yahoo.com")
-User.create(name: "Alice", username: "ally00n", password: "password", email: "alicemariethomas@site.edu")
-
-Review.create(title: "Amazing Food", content: "Amazing Food and Drinks!", rating: 5, user_id: 1, restaurant_id:1)
